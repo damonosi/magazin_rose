89ad2190635b38e5bdefb85a6f9e877bcde52bea
andrei <dam195@yahoo.com>
2022-07-13 10:08:45 +0300

    payment method

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
-import { SessionProvider } from "next-auth/react";
+import { SessionProvider, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 import Layout from "../components/Layout";
 
 import "../styles/globals.css";
@@ -9,11 +10,30 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }) {
     <SessionProvider session={session}>
       <StoreProvider>
         <Layout>
-          <Component {...pageProps} />
+          {Component.auth ? (
+            <Auth>
+              <Component {...pageProps} />
+            </Auth>
+          ) : (
+            <Component {...pageProps} />
+          )}
         </Layout>
       </StoreProvider>
     </SessionProvider>
   );
 }
-
+function Auth({ children }) {
+  const router = useRouter();
+  const { status } = useSession({
+    required: true,
+    onUnauthenticated() {
+      router.push("/unauthorized?message=login required");
+      console.log("Nu Esti logat");
+    },
+  });
+  if (status === "loading") {
+    return <div>Loading...</div>;
+  }
+  return children;
+}
 export default MyApp;
---
+++ pages/shipping.js
@@ -0,0 +1,132 @@
+import React, { useContext, useEffect } from "react";
+import CheckoutWizard from "../components/CheckoutWizard";
+import { useForm } from "react-hook-form";
+import { Store } from "../utils/Store";
+import Cookies from "js-cookie";
+import { useRouter } from "next/router";
+
+export default function ShippingScreen() {
+  const router = useRouter();
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+    setValue,
+    getValues,
+  } = useForm();
+  const { state, dispatch } = useContext(Store);
+  const { cart } = state;
+  const { shippingAddress } = cart;
+  useEffect(() => {
+    setValue("fullName", shippingAddress.fullName);
+    setValue("address", shippingAddress.address);
+    setValue("city", shippingAddress.city);
+    setValue("postalCode", shippingAddress.postalCode);
+    setValue("country", shippingAddress.country);
+  }, [setValue, shippingAddress]);
+  const submitHandler = ({ fullName, address, city, postalCode, country }) => {
+    dispatch({
+      type: "SAVE_SHIPPING_ADDRESS",
+      payload: { fullName, address, city, postalCode, country },
+    });
+    Cookies.set(
+      "cart",
+      JSON.stringify({
+        ...cart,
+        shippingAddress: {
+          fullName,
+          address,
+          city,
+          postalCode,
+          country,
+        },
+      }),
+    );
+    router.push("/payment");
+  };
+  return (
+    <div>
+      <CheckoutWizard activeStep={1} />
+      <form
+        onSubmit={handleSubmit(submitHandler)}
+        className="mx-auto max-w-screen-md"
+      >
+        <h1 className="mb-4 text-xl">Shipping Address</h1>
+        <div className="mb-4">
+          <label htmlFor="fullName">Full Name</label>
+          <input
+            className="w-full"
+            id="fullName"
+            autoFocus
+            {...register("fullName", { required: "Please enter full name" })}
+          />
+          {errors.fullName && (
+            <div className="text-red-500">{errors.fullName.message}</div>
+          )}
+        </div>
+        <div className="mb-4">
+          <label htmlFor="address">Address</label>
+          <input
+            className="w-full"
+            id="address"
+            autoFocus
+            {...register("address", {
+              required: "Please enter address",
+              minLength: { value: 3, message: "Address is more than 2 chars" },
+            })}
+          />
+          {errors.address && (
+            <div className="text-red-500">{errors.address.message}</div>
+          )}
+        </div>
+        <div className="mb-4">
+          <label htmlFor="city">City</label>
+          <input
+            className="w-full"
+            id="city"
+            autoFocus
+            {...register("city", {
+              required: "Please enter city",
+            })}
+          />
+          {errors.city && (
+            <div className="text-red-500">{errors.city.message}</div>
+          )}
+        </div>
+        <div className="mb-4">
+          <label htmlFor="city">Postal Code</label>
+          <input
+            className="w-full"
+            id="postalCode"
+            autoFocus
+            {...register("postalCode", {
+              required: "Please enter postalCode",
+            })}
+          />
+          {errors.postalCode && (
+            <div className="text-red-500">{errors.postalCode.message}</div>
+          )}
+        </div>
+        <div className="mb-4">
+          <label htmlFor="city">Country</label>
+          <input
+            className="w-full"
+            id="country"
+            autoFocus
+            {...register("country", {
+              required: "Please enter country",
+            })}
+          />
+          {errors.country && (
+            <div className="text-red-500">{errors.country.message}</div>
+          )}
+        </div>
+        <div className="mb-4 flex justify-between">
+          <button className="primary-button">Next</button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+ShippingScreen.auth = true;
