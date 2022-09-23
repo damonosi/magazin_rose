5558211857b2a792914487a66e7dfb1389712c5c
andrei <dam195@yahoo.com>
2022-09-23 20:20:05 +0300

    scos tsx

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,4 @@
-import Header from "./header/Header";
+import Header from "./header";
 import Footer from "./footer/Footer";
 import Head from "next/head";
 import { ToastContainer } from "react-toastify";
