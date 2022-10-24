d5c0168de42180cbfa21e14aa49838baf376cce1
andrei <dam195@yahoo.com>
2022-10-24 10:30:05 +0300

    rewrite middleware.js , transformat in  getToken din getSession

--- middleware.js
+++ middleware.js
@@ -1,25 +1,17 @@
-import { getSession } from "next-auth/react";
+import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
 
 export async function middleware(req) {
-	const requestForNextAuth = {
-		headers: {
-			cookie: req.headers.get("cookie"),
-		},
-	};
-
-	const session = await getSession({ req: requestForNextAuth });
+	const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
 
 	const user = {
-		name: session?.user?.name,
-		email: session?.user?.email,
-		image: session?.user?.image,
-		isAdmin: session?.user?.isAdmin ? "true" : "false",
+		name: token?.name,
+		email: token?.email,
+		image: token?.image,
+		isAdmin: token?.isAdmin ? "true" : "false",
 	};
 
-	console.log("este admin?", user.isAdmin);
-
-	if (!session) {
+	if (!token?.name) {
 		const signInPage = "/auth/login";
 		const signInUrl = new URL(signInPage, req.nextUrl.origin);
 		signInUrl.searchParams.append("callbackUrl", req.url);
@@ -30,7 +22,6 @@ export async function middleware(req) {
 		goHomeUrl.searchParams.append("callbackUrl", req.url);
 		return NextResponse.redirect(goHomeUrl);
 	} else {
-		// the user is not logged in, redirect to the sign-in page
 		return NextResponse.next();
 	}
 }
