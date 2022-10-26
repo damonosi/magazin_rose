188c60ce2c9b63f5b547021e7c851318b49fb8b4
andrei <dam195@yahoo.com>
2022-10-26 10:56:48 +0300

    api produse carusel

--- pages/api/dashboard/produse/produse-carusel.js
+++ pages/api/dashboard/produse/produse-carusel.js
@@ -6,20 +6,20 @@ const handler = async (req, res) => {
 	db.connect();
 	const toateProdusele = await Product.find().lean();
 	let produseCarusel = [];
-	const addItemsToProduseCarusel = () => {
-		toateProdusele.map((produsM) => {
-			const produsNou = {
-				name: produsM.name,
-				category: produsM.category,
-				image: produsM.image,
-				price: produsM.price,
-				slug: produsM.slug,
-				cantitate: produsM.cantitate,
-			};
-			produseCarusel.push(produsNou);
-		});
-	};
-	addItemsToProduseCarusel();
+	
+	toateProdusele.map((produsM) => {
+		const produsNou = {
+			name: produsM.name,
+			category: produsM.category,
+			image: produsM.image,
+			price: produsM.price,
+			slug: produsM.slug,
+			cantitate: produsM.cantitate,
+		};
+		produseCarusel.push(produsNou);
+	});
+	
+	
 	db.disconnect();
 	res.send(produseCarusel);
 };
